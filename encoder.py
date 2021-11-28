22db7e3f8efb1678c3dbdeb2d1ccb7e5123ec0e6
Anpopaicoconat <43803276+Anpopaicoconat@users.noreply.github.com>
2021-11-28 09:35:48 +0300

    Update encoder.py

--- encoder.py
+++ encoder.py
@@ -113,7 +113,7 @@ class PolyEncoder(BertPreTrainedModel):
             cand_emb = cand_emb.expand(batch_size, batch_size, cand_emb.shape[2]) # [bs, bs, dim]
             ctx_emb = self.dot_attention(cand_emb, embs, embs).squeeze() # [bs, bs, dim]
             dot_product = (ctx_emb*cand_emb).sum(-1) # [bs, bs]
-            C_negs = 0.2
+            C_negs = 1
             mask = (torch.eye(batch_size) + C_negs) - torch.eye(batch_size)*C_negs # [bs, bs]
             mask = mask.to(context_input_ids.device)
             loss = F.log_softmax(dot_product, dim=-1) * mask
