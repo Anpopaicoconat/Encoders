5f9e8df382dd249e760f579196daeb9ad40a2ea6
Anpopaicoconat <43803276+Anpopaicoconat@users.noreply.github.com>
2021-12-03 14:33:08 +0300

    Update and rename inference to inference.py

---
+++ inference.py
@@ -0,0 +1,63 @@
+import torch
+import transformers
+import pandas as pd
+
+import dataset
+import transform 
+import encoder 
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--bert_model", default='ckpt/pretrained/bert-small-uncased', type=str)
+    parser.add_argument("--architecture", required=True, type=str, help='[poly, bi, cross]')
+    parser.add_argument("--poly_m", default=0, type=int, help="Number of m of polyencoder")
+    parser.add_argument("--max_contexts_length", default=128, type=int)
+    parser.add_argument("--max_response_length", default=32, type=int)
+    parser.add_argument("--batch_size", default=32, type=int, help="Total batch size for training.")
+    parser.add_argument("--out_base", type=str, help="Path to computed cadidate base.")
+    args = parser.parse_args()
+    print(args)
+    
+    ## init dataset and bert model
+    tokenizer = transformers.TokeniserFast.from_pretrained(os.path.join(args.bert_model, "vocab.txt"), do_lower_case=True, clean_text=False)
+    context_transform = transform.SelectionJoinTransform(tokenizer=tokenizer, max_len=args.max_contexts_length)
+    response_transform = transform.SelectionSequentialTransform(tokenizer=tokenizer, max_len=args.max_response_length)
+    concat_transform = transform.SelectionConcatTransform(tokenizer=tokenizer, max_len=args.max_response_length+args.max_contexts_length)
+    
+    train_dataset = dataset.SelectionDataset(os.path.join(args.train_dir, 'train.txt'),
+                                                                  context_transform, response_transform, concat_transform, sample_cnt=None, mode=args.architecture)
+    train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=args.train_batch_size, collate_fn=train_dataset.batchify_join_str, shuffle=True, num_workers=0)
+    
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    bert_config = transformers.BertConfig.from_json_file(os.path.join(args.bert_model, 'config.json'))
+    bert_config.vocab_size = 119548
+    model_state_dict = torch.load('/content/bert/pytorch_model.bin', map_location="cpu")
+    bert = transformers.BertModel.from_pretrained('/content/bert/', state_dict=model_state_dict)
+    del model_state_dict
+    model.eval()
+    if args.architecture == 'poly':
+        model = encoder.PolyEncoder(bert_config, bert=bert, poly_m=args.poly_m, tokenizer=tokenizer)
+    elif args.architecture == 'bi':
+        model = encoder.BiEncoder(bert_config, bert=bert)
+    elif args.architecture == 'cross':
+        model = encoder.CrossEncoder(bert_config, bert=bert)
+    else:
+        raise Exception('Unknown architecture.')
+    model.resize_token_embeddings(len(tokenizer)) 
+    model.to(device)
+    
+    responses_base = []
+    
+    for step, batch in enumerate(train_dataloader):
+        batch = tuple(t.to(device) for t in batch[:2])
+        if args.architecture == 'cross':
+            text_token_ids_list_batch, text_input_masks_list_batch = batch
+            loss = model(text_token_ids_list_batch, text_input_masks_list_batch)
+        else:
+            context_token_ids_list_batch, context_input_masks_list_batch = batch
+            out = model(context_token_ids_list_batch, context_input_masks_list_batch)
+            responses_base += out
+    responses_base = pd.DataFrame(responses_base)
+    responses_base.to_csv(parser.out_base)
+    
