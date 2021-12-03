e5395308730978df8e006c41ff07650272b3f085
Anpopaicoconat <43803276+Anpopaicoconat@users.noreply.github.com>
2021-12-03 18:48:50 +0300

    Update inference.py

--- inference.py
+++ inference.py
@@ -64,7 +64,7 @@ if __name__ == '__main__':
                 loss = model(text_token_ids_list_batch, text_input_masks_list_batch)
             else:
                 context_token_ids_list_batch, context_input_masks_list_batch = batch
-                out = model(context_token_ids_list_batch, context_input_masks_list_batch).detach().numpy()
+                out = model(context_token_ids_list_batch, context_input_masks_list_batch).cpu().detach().numpy()
                 for i in out:
                     writer.writerow(i)
     
