a7860c8300addd4d2eb21f928afe8e21df6b64ee
Anpopaicoconat <43803276+Anpopaicoconat@users.noreply.github.com>
2021-12-03 14:58:07 +0300

    Update inference.py

--- inference.py
+++ inference.py
@@ -35,7 +35,7 @@ if __name__ == '__main__':
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     bert_config = transformers.BertConfig.from_json_file(os.path.join(args.bert_model, 'config.json'))
     bert_config.vocab_size = 119548
-    previous_model_file = os.path.join(args.bert_model, "pytorch_model.bin")
+    previous_model_file = os.path.join(args.bert_model, "pytorch_model.bin") 
     print('Loading parameters from', previous_model_file)
     model_state_dict = torch.load(previous_model_file, map_location="cpu")
     bert = transformers.BertModel.from_pretrained('/content/bert/', state_dict=model_state_dict)
