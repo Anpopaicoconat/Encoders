4b19efe934e71c616bea25c088256fbe9093ad72
Anpopaicoconat <43803276+Anpopaicoconat@users.noreply.github.com>
2021-12-04 18:12:52 +0300

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -6,16 +6,11 @@ with open('token.txt', 'r') as token:
     TOKEN = token.read()
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
-@dp.message_handler(commands=['start', 'help'])
-async def send_welcome(msg: types.Message):
-    await msg.reply_to_message('Я бот. Приятно познакомиться')
     
 @dp.message_handler(content_types=['text'])
 async def get_text_messages(msg: types.Message):
-   if msg.text.lower() == 'привет':
-       await msg.answer('Привет!')
-   else:
-       await msg.answer('Не понимаю, что это значит.')
+    inp = msg.text
+    await msg.answer(inp)
       
 if __name__ == '__main__':
    executor.start_polling(dp)
