1b417b35f295e4bf203bab03237366c19ed5e678
Anpopaicoconat <43803276+Anpopaicoconat@users.noreply.github.com>
2021-12-04 16:55:41 +0300

    Update get_base.py

--- get_base.py
+++ get_base.py
@@ -66,7 +66,7 @@ if __name__ == '__main__':
                 out = model(context_token_ids_list_batch, context_input_masks_list_batch).cpu().detach().tolist()
                 context_token_ids_list_batch = context_token_ids_list_batch.cpu().detach().tolist()
                 for ids, embd in zip(context_token_ids_list_batch, out):
-                    string = '{}|||{}'\n''.format(' '.joinn(ids), ' '.joinn(embd))
+                    string = '{}|||{}'\n''.format(' '.join(ids), ' '.join(embd))
     
                     base.write(string)
             if step%10==0:
