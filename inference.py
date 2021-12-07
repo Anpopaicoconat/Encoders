5e6bf71ddb7eb6747ce71f3217040836dffd9d18
Anpopaicoconat <43803276+Anpopaicoconat@users.noreply.github.com>
2021-12-07 23:46:00 +0300

    Update inference.py

--- inference.py
+++ inference.py
@@ -119,7 +119,7 @@ if __name__ == '__main__':
                     embd_batch.append(embd)
                     ids_batch.append(ids)
                     if step % args.batch_size == 0:
-                        embd_batch = torch.tensor(embd_batch).to(device)
+                        embd_batch = torch.tensor(embd_batch, dtype=torch.float).to(device)
                         print(embd_batch)
                         out = model(context_token_ids_list_batch, context_input_masks_list_batch, embd_batch, mod='inference').cpu().detach().numpy()
                         outmax = max(out)
