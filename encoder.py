e86f9ae259279b20c1f8d1cd49e6810c250d2320
Anpopaicoconat <43803276+Anpopaicoconat@users.noreply.github.com>
2021-12-09 20:43:04 +0300

    Update encoder.py

--- encoder.py
+++ encoder.py
@@ -103,10 +103,8 @@ class PolyEncoder(BertPreTrainedModel):
         # context encoder
         if context_input_ids is not None:
             if mod == 'inference2': #подаем батч с предложением пользователя и расширяем до размера батча-числа кандидатов
-                print(context_input_masks.shape)
                 context_input_ids = context_input_ids.expand(batch_size, context_input_ids.shape[-1])
                 context_input_masks = context_input_masks.expand(batch_size, context_input_ids.shape[-1])
-                print(context_input_masks.shape)
             ctx_out = self.bert(context_input_ids, context_input_masks)[0]  # [bs, length, dim]
             poly_code_ids = torch.arange(self.poly_m, dtype=torch.long).to(context_input_ids.device)
             poly_code_ids = poly_code_ids.unsqueeze(0).expand(batch_size, self.poly_m)
