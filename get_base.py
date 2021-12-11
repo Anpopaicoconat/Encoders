366600c57a42d4ab462cc4f26df7ef21351bda43
Anpopaicoconat <43803276+Anpopaicoconat@users.noreply.github.com>
2021-12-11 13:47:47 +0300

    Update get_base.py

--- get_base.py
+++ get_base.py
@@ -93,7 +93,7 @@ if __name__ == '__main__':
 
         elif args.architecture == 'bi':
             context_token_ids_list_batch, context_input_masks_list_batch = batch
-            out = model(context_token_ids_list_batch, context_input_masks_list_batch).cpu().detach().tonumpy()
+            out = model(responses_input_ids=candidates_token_ids_list_batch, responses_input_masks=candidates_input_masks_list_batch, mod='get_base').cpu().detach().tonumpy()
             context_token_ids_list_batch = context_token_ids_list_batch.cpu().detach().tonumpy()
             
             if args.faiss:
