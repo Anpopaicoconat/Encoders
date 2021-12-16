8049160069cb694c173109f70e4a02f3d212ad42
Anpopaicoconat <43803276+Anpopaicoconat@users.noreply.github.com>
2021-12-16 22:26:54 +0300

    Update encoder.py

--- encoder.py
+++ encoder.py
@@ -60,8 +60,8 @@ class BiEncoder(BertPreTrainedModel):
             dot_product = torch.cdist(context_vec, responses_vec).squeeze()
             print(dot_product.size())
             #print('context_vec', context_vec.size(), 'responses_vec', responses_vec.permute(0, 2, 1).size())
-            dot_product = torch.matmul(context_vec, responses_vec.permute(0, 2, 1)).squeeze()
-            print(dot_product.size())
+            #dot_product = torch.matmul(context_vec, responses_vec.permute(0, 2, 1)).squeeze()
+            #print(dot_product.size())
             return dot_product
 
 
