bd0f1666ca955a274fd8763c20c1867d5a7062c7
Anpopaicoconat <43803276+Anpopaicoconat@users.noreply.github.com>
2021-12-16 22:23:30 +0300

    Update encoder.py

--- encoder.py
+++ encoder.py
@@ -56,11 +56,11 @@ class BiEncoder(BertPreTrainedModel):
             return loss
 
         else:
-            context_vec = context_vec.unsqueeze(1).expand(responses_vec.size())
-            dot_product = torch.cdist(context_vec, responses_vec).squeeze()
-            print('context_vec', context_vec.size(), 'responses_vec', responses_vec.permute(0, 2, 1).size())
-            print('og', torch.matmul(context_vec, responses_vec.permute(0, 2, 1)).squeeze().size(), 'new', dot_product.size())
-            #dot_product = torch.matmul(context_vec, responses_vec.permute(0, 2, 1)).squeeze()
+            context_vec = context_vec.unsqueeze(1)#.expand(responses_vec.size())
+            #dot_product = torch.cdist(context_vec, responses_vec).squeeze()
+            #print('context_vec', context_vec.size(), 'responses_vec', responses_vec.permute(0, 2, 1).size())
+            #print('og', torch.matmul(context_vec, responses_vec.permute(0, 2, 1)).squeeze().size(), 'new', dot_product.size())
+            dot_product = torch.matmul(context_vec, responses_vec.permute(0, 2, 1)).squeeze()
             print(dot_product.size())
             return dot_product
 
